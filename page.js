1001ea1a1dfc55a793fb1cff3ac1107bce19fc30
Md. Moshiur Rahman <107361922+MdMoshiurtime@users.noreply.github.com>
2025-12-03 18:09:00 +0600

    Add main components to page.js

---
+++ page.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Activities from "./components/Activities";
+import Awards from "./components/Awards";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Activities />
+      <Awards />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
